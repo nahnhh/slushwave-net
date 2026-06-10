bc1476dbd61477505e6f2140f85eaa1f3b7aa5de
Hân <166970040+nahnhh@users.noreply.github.com>
2026-06-10 17:06:44 +0700

    Good to run `parse_album_page.py`. Return results in json format!

--- parse_album_page.py
+++ parse_album_page.py
@@ -4,10 +4,10 @@ import os
 import random
 import re
 import hashlib
-from datetime import timedelta
+from tqdm import tqdm
 from typing import Any, Dict, List, Optional
 import pandas as pd
-from datetime import timedelta
+from datetime import timedelta, datetime
 from firefox_profiles import FINGERPRINTS
 from bs4 import BeautifulSoup
 from async_tls_client import AsyncSession
@@ -32,7 +32,7 @@ IMAGES_DIR = BASE_DIR / 'images'
 #STARTUP TEST PROFILES
 TEST_URL = "https://giftsfromhome.bandcamp.com/album/-"
 
-async def test_profile(profile):
+async def _test_profile(profile):
 	s = AsyncSession(client_identifier=profile)
 	try:
 		r = await s.get(TEST_URL)
@@ -43,20 +43,17 @@ async def test_profile(profile):
 	except Exception:
 		return profile, True
 	
-async def get_good_profiles():
-		tasks = [test_profile(profile) for profile in FINGERPRINTS]
-		results = await asyncio.gather(
-				*tasks,
-				return_exceptions=False
-		)
-		failed = []
-		good = []
-		for profile, challenged in results:
-				if challenged:
-						failed.append(profile)
-				else:
-						good.append(profile)
-		return good, failed
+async def _get_good_profiles():
+	tasks = [_test_profile(profile) for profile in FINGERPRINTS]
+	results = await asyncio.gather(*tasks, return_exceptions=False)
+	failed = []
+	good = []
+	for profile, challenged in results:
+		if challenged:
+			failed.append(profile)
+		else:
+			good.append(profile)
+	return good, failed
 
 class BrowserSession:
 	def __init__(self, ok_clients: list):
@@ -103,8 +100,11 @@ def nozero(text: Any) -> str:
 		return re.sub(r"[\u200b\u200c\u200d\ufeff]", "", text)
 
 class Scraper:
-	def __init__(self, ok_clients: list):
-		self.s = BrowserSession(ok_clients)
+	def __init__(self, session_or_clients):
+		if isinstance(session_or_clients, BrowserSession):
+			self.s = session_or_clients
+		else:
+			self.s = BrowserSession(session_or_clients)
 	
 	async def get_art_id(self, url):
 		r = await self.s.get(url)
@@ -119,26 +119,16 @@ class Scraper:
 
 		return art_id, img_hash
 
-	async def scrape_album_page(soup: list) -> Dict[str, Any]:
-		"""
-		Fetch one Bandcamp album page and return structured album metadata.
-
-		The returned dictionary includes:
-		- album name, artist, url, image
-		- release dates and tags
-		- every track url and duration
-		- total runtime
-		"""
-
-		if (len(soup) == 1 or isinstance(soup,str)):
-			soup = [soup]
-
+	async def scrape_album_page(self, soup) -> Dict[str, Any]:
+		"""Fetch album page and returns all required metadata."""
+		soup_title = nozero(soup.title.get_text(strip=True))
 		schema = json.loads(soup.select("script[type='application/ld+json']")[0].get_text(strip=True))
-		tralbum_tag = soup[0].select_one("[data-tralbum]")
+		tralbum_tag = soup.select_one("[data-tralbum]")
 		current = json.loads(tralbum_tag["data-tralbum"])['current']
 		track_info = json.loads(tralbum_tag["data-tralbum"])['trackinfo']
 
 		# LD+JSON: album name, artist, number of tracks, keywords/tags
+		url = nozero(schema['@id'])
 		album_name = nozero((schema['name'] or current['title'] or ""))
 		artist_name = nozero((schema['byArtist']['name'] or current['artist'] or ""))
 		num_tracks = nozero((schema['numTracks'] or current['track_count'] or ""))
@@ -162,11 +152,11 @@ class Scraper:
 
 		# All track images
 		results = await asyncio.gather(
-		*(get_art_id(url, session) for url in track_url_df["url"])
+		*(self.get_art_id(url) for url in track_url_df["url"])
 		)
 
 		track_art_df = pd.DataFrame(results, columns=["art_id", "img_hash"]).drop_duplicates(subset=["img_hash"])
-		log.info(f"Total unique image hashes: {track_art_df['img_hash'].nunique()}")
+		log.info(f"{soup_title} - Unique image hashes: {track_art_df['img_hash'].nunique()}")
 		
 
 		result = {
@@ -181,25 +171,34 @@ class Scraper:
 				"new_date": current.get("new_date") or "",
 				"mod_date": current.get("mod_date") or "",
 				"album_art_id": current.get("art_id"),
-				"track_art_id": track_art_df.get("art_id")
+				"track_art_id": track_art_df.get("art_id").tolist()
 		}
 
 		return result
 
+# =======
+# OKAY!!!
+# =======
 
-async def main():
+async def get_ok_clients():
 	#STARTUP TEST CLIENTS
-	CACHE_FILE = Path("good_profiles.json")
-	if CACHE_FILE.exists():
-		with open(CACHE_FILE, "r") as f:
-				OK_CLIENTS = json.load(f)
+	force = input("Retest client identifiers? [y/N]:").strip().lower() in ('y', 'yes')
+	cache_file = Path("good_profiles.json")
+	if not force and cache_file.exists():
+		with open(cache_file, "r") as f:
+				ok_clients = json.load(f)
 		print("Good Profiles (old cache):")
 	else:
-		OK_CLIENTS, _ = await get_good_profiles()
-		with open(CACHE_FILE, "w") as f:
-				json.dump(OK_CLIENTS, f)
+		if not cache_file.exists():
+			print("CACHE_FILE doesn't exist. Starting test...")
+		ok_clients, _ = await _get_good_profiles()
+		with open(cache_file, "w") as f:
+				json.dump(ok_clients, f)
 		print("Good Profiles (new cache):")
-	print(OK_CLIENTS)
+	print(ok_clients)
+
+async def main():
+	OK_CLIENTS = await get_ok_clients()
 
 	url1 = 'https://daysofblue.bandcamp.com/album/--12'
 	url2 = 'https://noproblematapes.bandcamp.com/album/--89'
@@ -208,8 +207,10 @@ async def main():
 
 	random.seed(42)
 	s = BrowserSession(OK_CLIENTS)
+	scraper = Scraper(s)
 	failed = []
 	soups = []
+	results = []
 
 	for url in urls:
 		r = await s.get(url)
@@ -225,7 +226,17 @@ async def main():
 		soups.append(soup)
 
 	print("Albums fetched:")
-	print([soup.title.get_text(strip=True) for soup in soups])
+	print([nozero(soup.title.get_text(strip=True)) for soup in soups])
+
+	for soup in tqdm(soups, desc="Extracting metadata from albums..."):
+		result = await scraper.scrape_album_page(soup)
+		results.append(result)
+
+	timestamp = datetime.now().strftime("%y%m%dT%H%M")
+	results_file = Path(f"results_{timestamp}.json")
+	with open(results_file, "w", encoding="utf-8") as f:
+			json.dump(results, f, indent=2, ensure_ascii=False)
+	log.info(f"Saved to {results_file}")
 
 if __name__ == "__main__":
 	asyncio.run(main())
